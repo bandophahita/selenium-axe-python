61bc58a0255ad73eb09f978d2b812bfd86f9a955
Marcel Wilson <trenchrats@gmail.com>
2023-07-28 11:48:03 -0500

    refactor cleanup

--- axe_selenium_python/tests/test_axe.py
+++ axe_selenium_python/tests/test_axe.py
@@ -3,34 +3,104 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
-from os import getcwd, getenv, path
+import os
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 from ..axe import Axe
 
-_DEFAULT_TEST_FILE = path.join(path.dirname(__file__), "test_page.html")
+_DEFAULT_TEST_FILE = os.path.join(os.path.dirname(__file__), "test_page.html")
 
 
 @pytest.fixture
 def firefox_driver():
-    driver = webdriver.Firefox()
+    enable_log_driver = False
+    log_dir: str = "./logs"
+    driver_path: str | None = None
+
+    options = webdriver.FirefoxOptions()
+    options.set_capability("unhandledPromptBehavior", "ignore")
+
+    # profile settings
+    options.set_preference("app.update.auto", False)
+    options.set_preference("app.update.enabled", False)
+    options.set_preference("network.prefetch-next", False)
+    options.set_preference("network.dns.disablePrefetch", True)
+    options.add_argument("--headless")
+
+    logpath = "/dev/null"
+    options.log.level = "fatal"  # type: ignore[assignment]
+    if enable_log_driver:
+        lp = os.path.abspath(os.path.expanduser(log_dir))
+        logpath = os.path.join(lp, "geckodriver.log")
+
+    if driver_path:
+        service = FirefoxService(executable_path=driver_path, log_path=logpath)
+    else:
+        service = FirefoxService(log_path=logpath)
+
+    driver = webdriver.Firefox(service=service, options=options)
     yield driver
     driver.close()
 
 
 @pytest.fixture
 def chrome_driver():
-    opts = webdriver.ChromeOptions()
-    opts.headless = True
-    opts.add_argument("--no-sandbox")
-    driver_path = getenv("CHROMEDRIVER_PATH")
-    driver = (
-        webdriver.Chrome(options=opts, executable_path=driver_path)
-        if driver_path
-        else webdriver.Chrome(options=opts)
+    enable_log_driver = False
+    log_dir: str = "./logs"
+
+    opts = (
+        "--disable-extensions",
+        "--allow-running-insecure-content",
+        "--ignore-certificate-errors",
+        "--disable-single-click-autofill",
+        "--disable-autofill-keyboard-accessory-view[8]",
+        "--disable-full-form-autofill-ios",
+        # https://bugs.chromium.org/p/chromedriver/issues/detail?id=402#c128
+        # "--dns-prefetch-disable",
+        "--disable-infobars",
+        # chromedriver crashes without these two in linux
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
     )
+
+    options = webdriver.ChromeOptions()
+    for opt in opts:
+        options.add_argument(opt)
+
+    options.headless = True
+    driver_path = os.getenv("CHROMEDRIVER_PATH")
+
+    logging_prefs = {"browser": "OFF", "performance": "OFF", "driver": "OFF"}
+
+    args: list | None = None
+    logpath = None
+    if enable_log_driver:
+        lp = os.path.abspath(os.path.expanduser(log_dir))
+        logpath = os.path.join(lp, "chromedriver.log")
+        args = [
+            # "--verbose"
+        ]
+        logging_prefs["driver"] = "ALL"
+
+    options.set_capability("goog:loggingPrefs", logging_prefs)
+
+    if driver_path:
+        service = ChromeService(
+            executable_path=driver_path,
+            service_args=args,
+            log_path=logpath,
+        )
+    else:
+        service = ChromeService(
+            service_args=args,
+            log_path=logpath,
+        )
+
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.close()
 
@@ -69,7 +139,7 @@ def _perform_axe_run(driver):
 def test_write_results_to_file(tmpdir, mocker):
     axe = Axe(mocker.MagicMock())
     data = {"testKey": "testValue"}
-    filename = path.join(str(tmpdir), "results.json")
+    filename = os.path.join(str(tmpdir), "results.json")
 
     axe.write_results(data, filename)
 
@@ -82,12 +152,12 @@ def test_write_results_to_file(tmpdir, mocker):
 def test_write_results_without_filepath(mocker):
     axe = Axe(mocker.MagicMock())
     data = {"testKey": "testValue"}
-    cwd = getcwd()
-    filename = path.join(cwd, "results.json")
+    cwd = os.getcwd()
+    filename = os.path.join(cwd, "results.json")
 
     axe.write_results(data, filename)
     with open(filename) as f:
         actual_file_contents = json.loads(f.read())
 
     assert data == actual_file_contents
-    assert path.dirname(filename) == cwd
+    assert os.path.dirname(filename) == cwd
